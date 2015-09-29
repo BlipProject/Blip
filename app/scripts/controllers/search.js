88d560bc7d515add426520160a088a6efb91f1bb
Brian Geraghty <brian_geraghty@outlook.com>
2015-09-29 21:57:06 +0100

    results returning to console

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -5,7 +5,8 @@ angular.module('blipApp')
 	.controller('LocationSearchCtrl', ['$http','$scope', function ($http,$scope) {
 		var setLocation = "";
 		var searchResult;
-		this.searchResult="";
+		this.searchResult=[{"LocationID":"1","LocationName":"Sligo","LocationDescription":"Location1","LocationLat":"1234","LocationLong":"5678","LocName":"Aldi"},{"LocationID":"3","LocationName":"Sligo","LocationDescription":"Location3","LocationLat":"2568","LocationLong":"2548","LocName":"Tesco"}];
+
 
 		//TODO Change post URL to reletive link
 		this.setLocation = function(searchCity){
