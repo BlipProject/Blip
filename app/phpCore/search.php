6a5d784a6794a7b3e458df56f51026a838b0d46d
Brian Geraghty <brian_geraghty@outlook.com>
2015-10-03 23:56:34 +0100

    Search results now populating based on GPS location

--- app/phpCore/search.php
+++ app/phpCore/search.php
@@ -19,12 +19,11 @@
 
 	//Start Script
 
-	//Input variable from Angular
+	//Input variables from Angular
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
 	$longitude = $request->longitude;
 	$latitude = $request->latitude;
-	echo($longitude);
 
 	$conn = mysqli_connect($servername, $username, $password, $db);
 
@@ -36,7 +35,7 @@
 		global $conn;
 
 		$search = mysqli_query($conn, 
-	    "CALL 30Nearest('$MapLat','$MapLong')") or die("Query fail: " . mysqli_error($conn));
+	    "CALL 30Nearest('$lat','$long')") or die("Query fail: " . mysqli_error($conn));
 
 		while($row = $search->fetch_array(MYSQL_ASSOC)) {
             $resultsArray[] = $row;
--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -2,42 +2,53 @@
 
 angular.module('blipApp')
 	
-	.controller('LocationSearchCtrl', ['$http','$scope', function ($http,$scope) {
+	.controller('LocationSearchCtrl', ['$http','$scope','$timeout', function ($http,$scope,$timeout) {
 
-		//Stores geolocation data to send to phph script
+		//Stores geolocation data to send to php script
 		var data;
 		//Store search result returned from server
-		var searchResult;
+		//var searchResult;
+		$scope.searchResult;
+		
+		$scope.getLocation = function(){
 
-		if (navigator.geolocation) {
-		    navigator.geolocation.getCurrentPosition(function(position){
-		      $scope.$apply(function(){
-		        $scope.position = position;
-		        data = [{
-		        	longitude:position.coords.longitude,
-		        	latitudelatitude:position.coords.latitude
-		        }];
+			var positionOptions = {
+			  enableHighAccuracy: false,
+			  timeout: 1000,
+			  maximumAge: 500
+			};
 
-		        getLocationResults(data);
+			if (navigator.geolocation) {
+			    navigator.geolocation.getCurrentPosition(function(position,positionOptions){
+					enableHighAccuracy: false,
+					$scope.$apply(function(){
+			        	$scope.position = position;
+				        data = {
+				        	longitude : position.coords.longitude,
+				        	latitude : position.coords.latitude
+				        };
+			        	getLocationResults(data);
+					});
+			    });
+			}
+		};
 
-		        console.log(data);
-		      });
-		    });
-		}
 
 		//TODO Change post URL to reletive link
 		var getLocationResults = function(data){
-			searchResult = $http.post('http://localhost/blip/app/phpCore/search.php', data)
+			var callSearch = $http.post('http://localhost/blip/app/phpCore/search.php', data)
 		        .success(function(data, status, headers, config)
 		        {
-				    console.log( status + ' - ' + "Success");
-		            searchResult = JSON.stringify(data);
-		            //console.log(searchResult);          
+				    console.log( status + ' - ' + "Success");          
 	            })
 		        .error(function(data, status, headers, config)
 		        {
 		            console.log('error');
-		        });
+		        })
+		        .then(function(response) {
+			        $scope.searchResult = response.data;
+			        console.log(JSON.stringify(response.data));
+				});
 		};
 }]);
 
