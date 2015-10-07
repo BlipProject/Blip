60ef2aa3f3b4934348fc8e2491e970bd0e2b8d70
Brian Geraghty <brian_geraghty@outlook.com>
2015-10-07 18:29:25 +0100

    Sidebar Working

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -3,18 +3,6 @@
 angular.module('blipApp')
 	.controller('NavCtrl',['$scope', function ($scope) 
 	{
-		$scope.classCheck = false;
-	  	$scope.setSideClass = function(){
-		    if($scope.classCheck === false)
-		    {
-		    	$scope.classCheck = true;
-		    	console.log($scope.classCheck);
-			}
-			else
-			{
-				$scope.classCheck = false;
-				console.log($scope.classCheck);
-			}
-		};
+
 	}]);
 
