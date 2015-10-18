dc4dc8d3fa8f8a408abe4669173b1c828aeff1a8
Fergal Gaffney <s00147036@mail.itsligo.ie>
2015-10-18 18:17:23 +0100

    Moved code for Business Location from map.js to register_business.js

--- app/scripts/controllers/register_business.js
+++ app/scripts/controllers/register_business.js
@@ -8,5 +8,46 @@
  * Controller of the blipApp
  */
 angular.module('blipApp')
-  .controller('RegisterCompanyCtrl', function () {
-  });
+  .controller('RegisterBusinessCtrl', ['$http','$scope', 'uiGmapGoogleMapApi', function ($http,$scope,uiGmapGoogleMapApi) {
+
+  	uiGmapGoogleMapApi.then(function(maps) {
+
+  	});
+
+  	$scope.getCoordinates = function(){
+		  $scope.geodata = {};
+		  $scope.queryResults = {};
+		  $scope.queryError = {};
+		  $scope.address = document.getElementById('busaddress').value;
+		  console.log($scope.address);
+
+		  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
+		            $scope.address + '&key=AIzaSyCn9zl42b2gnUt92A7v_OcAJB4OUem-zbM')
+		    .then(function(_results){
+		       console.log(_results.data);
+
+		       $scope.queryResults = _results.data.results;
+		       $scope.geodata = $scope.queryResults[0].geometry;
+
+		         var buslatlng = $scope.queryResults[0].geometry.location;
+		  		console.log(buslatlng);
+
+		  		$scope.busmarker =  {
+		        id: 5,
+		        coords: {
+		            latitude: buslatlng.lat,
+		            longitude: buslatlng.lng
+		        	},
+		        data: 'newbusinesslocation',
+		        animation: google.maps.Animation.DROP//not working
+		    	};
+
+
+		     }, 
+		     function error(_error){
+		        $scope.queryError = _error;
+		        console.log($scope.queryError);
+		     })
+
+		}
+  }]);
