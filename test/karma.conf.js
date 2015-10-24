d41a894422d3fb090e77a07686326dca9c193fa9
Fergal Gaffney <s00147036@mail.itsligo.ie>
2015-10-24 19:55:11 +0100

    Added bower dependancy for Bootstrap Tokenfield

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/bootstrap-tokenfield/dist/bootstrap-tokenfield.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
