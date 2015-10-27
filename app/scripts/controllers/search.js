3aaa35fb86ea4e2ddb0e200f03a4f3bbe06be57a
Brian Geraghty <brian_geraghty@outlook.com>
2015-10-27 03:20:30 +0000

    Fixed images not being responsive on search page

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -45,7 +45,7 @@ angular.module('blipApp')
 				        	longitude : position.coords.longitude,
 				        	latitude : position.coords.latitude
 				        };
-			        	getLocationResults(data)
+			        	getLocationResults(data);
 					});
 			    });
 			}
@@ -56,7 +56,7 @@ angular.module('blipApp')
 		///////////
 		//TESTING URL http://localhost/blip/app/phpCore/search.php
 		var getLocationResults = function(data){
-			var callSearch = $http.post('../phpCore/search.php', data)
+			var callSearch = $http.post('http://localhost/blip/app/phpCore/search.php', data)
 		        .success(function(data, status, headers, config)
 		        {
 		        	$scope.searchResult = data;
