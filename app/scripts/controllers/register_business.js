d9b0244b99ab95058b62318e1cbec103953156f8
Fergal Gaffney <s00147036@mail.itsligo.ie>
2015-10-28 20:26:29 +0000

    Working on form layout

--- app/scripts/controllers/register_business.js
+++ app/scripts/controllers/register_business.js
@@ -18,6 +18,18 @@ angular.module('blipApp')
   	var busLat;
   	var busLng;
 
+  	$scope.map = {
+  		center: { latitude: 54.276293, longitude: -8.476524},
+		zoom: 16
+  	};
+  	$scope.busmarker =  {
+		id: 5,
+		coords: {
+			latitude: 54.276293,
+			longitude: -8.476524
+		}
+	};
+
 	///////////
 	//IMPORTANT Change post URL to reletive link before build... '../phpCore/get_categories.php'
 	///////////
@@ -65,7 +77,6 @@ angular.module('blipApp')
   	};
 
   	uiGmapGoogleMapApi.then(function(maps) {
-
   	});
 
   	$scope.getCoordinates = function(busAddress, busAddress2, busCity, busCountry){
@@ -90,7 +101,7 @@ angular.module('blipApp')
 
 		$scope.map ={
 			center: { latitude: buslatlng.lat, longitude: buslatlng.lng},
-			zoom: 15
+			zoom: 16
 		};
 		$scope.busmarker =  {
 			id: 5,
