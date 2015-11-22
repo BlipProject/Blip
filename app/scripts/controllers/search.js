136bcc3878b5b64856217b184a835da95183254d
Brian Geraghty <brian_geraghty@outlook.com>
2015-11-22 22:50:55 +0000

    Fixed scroll issue with nationality dropdown

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -18,6 +18,7 @@ angular.module('blipApp')
 		
 		$scope.getLocationNewCountry = function(newCountry){
 			$scope.userNationality = newCountry;
+			console.log(newCountry);
 			$scope.getLocation();
 		};
 
