7bd67fd2e5314541c50375bf2d53b4489b83c3dd
Fergal Gaffney <s00147036@mail.itsligo.ie>
2016-02-06 01:12:19 +0000

    Added setResultClass from search.js

--- app/scripts/controllers/userLocations.js
+++ app/scripts/controllers/userLocations.js
@@ -15,9 +15,42 @@ angular.module('blipApp')
 				{
 					console.log("Success");
 					$scope.searchResult = response.data;
-				})
+				});
 		};
 
 		console.log($scope.searchResult);
+
+		$scope.setResultClass = function(classIn) {
+            switch (classIn) {
+                case 'Bar':
+                    {
+                        $scope.typeHeadClass = "result-header-bar";
+                        $scope.setIconClass = "fa fa-glass fa-lg";
+                        return "result-hover-button-bar";
+                    }
+                case 'Restaurant':
+                    {
+                        $scope.typeHeadClass = "result-header-restaurant";
+                        $scope.setIconClass = 'fa fa-cutlery fa-lg';
+                        return "result-hover-button-restaurant";
+                    }
+                case 'Supermarket':
+                    {
+                        $scope.typeHeadClass = "result-header-shop";
+                        $scope.setIconClass = 'fa fa-shopping-cart fa-lg';
+                        return "result-hover-button-shop";
+                    }
+                case 'Other':
+                    {
+                        $scope.typeHeadClass = "result-header-other";
+                        $scope.setIconClass = "fa fa-ellipsis-h fa-lg";
+                        return "result-hover-button-other";
+                    }
+                default:
+                    {
+                        return "";
+                    }
+            }
+        };
 
 	}]);
