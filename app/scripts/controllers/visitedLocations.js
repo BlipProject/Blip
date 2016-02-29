70ff4e6ba2b5f7bbbc1c9ddc50fd3aab6a9b034d
Fergal Gaffney <s00147036@mail.itsligo.ie>
2016-02-29 03:43:26 +0000

    Visited Locations being displayed

--- app/scripts/controllers/visitedLocations.js
+++ app/scripts/controllers/visitedLocations.js
@@ -5,6 +5,89 @@ angular.module('blipApp')
 	'$scope',
 	function($http, $scope) {
 
+		$scope.visitedLocations = "";
+		$scope.filterVisitedLocations = [];
+		$scope.showLoadingAnimation = true;
+
+		var user = {
+			ID: 321
+		};
+
+		$scope.getVisitedLocations = function() {
+			$http.post('http://localhost/blip/app/phpCore/get_visited_locations.php', user)
+				.then(function(response)
+				{
+					$scope.visitedLocations = response.data;
+					$scope.filterVisitedLocations = $scope.visitedLocations;
+					console.log($scope.visitedLocations);
+				    $scope.showLoadingAnimation = false;
+				    $(".visited-locations").removeClass("hide");
+				});
+		};
+
+        $scope.setFilterSetClass = function(filter, index) {
+            getFilter(filter);
+            setQuickFilterClass(index);
+        };
+
+        //Called to return filtered content
+        //If search result matches the filter button it gets pushed into 'filterVisitedLocations' array
+        //else 'filterVisitedLocations' equals the content that was origionaly returned from the server
+        var getFilter = function(filter) {
+            if (filter !== "All") {
+                $scope.filterVisitedLocations = [];
+                angular.forEach($scope.visitedLocations, function(value) {
+                    if (value.CategoryName === filter) {
+                        $scope.filterVisitedLocations.push(value);
+                    }
+                });
+            } else {
+                $scope.filterVisitedLocations = $scope.visitedLocations;
+            }
+        };
+
+        //Sets active class on selected filter button
+        $scope.activeFilter = 0;
+        var setQuickFilterClass = function(type) {
+            $scope.activeFilter = type;
+        };
+
+		$scope.typeHeadClass = " ";
+        $scope.setIconClass = " ";
+		$scope.setResultClass = function(classIn) {
+            switch (classIn) {
+                case 'Bar':
+                    {
+                        $scope.typeHeadClass = "result-header-bar";
+                        $scope.setIconClass = "fa fa-glass fa-lg";
+                        return "result-hover-button-bar";
+                    }
+                case 'Restaurant':
+                    {
+                        $scope.typeHeadClass = "result-header-restaurant";
+                        $scope.setIconClass = 'fa fa-cutlery fa-lg';
+                        return "result-hover-button-restaurant";
+                    }
+                case 'Supermarket':
+                    {
+                        $scope.typeHeadClass = "result-header-shop";
+                        $scope.setIconClass = 'fa fa-shopping-cart fa-lg';
+                        return "result-hover-button-shop";
+                    }
+                case 'Other':
+                    {
+                        $scope.typeHeadClass = "result-header-other";
+                        $scope.setIconClass = "fa fa-ellipsis-h fa-lg";
+                        return "result-hover-button-other";
+                    }
+                default:
+                    {
+                        return "";
+                    }
+            }
+        };
+
+        $scope.init = getFilter('All');
 	}])
 	.directive('profileMap', [function() {
 
@@ -43,8 +126,8 @@ angular.module('blipApp')
 
 			    	map.dragMap = false;
 
-			    	var div = document.getElementById("visitedCountriesMap");
-					map.write(div);
+			    	var mapDiv = document.getElementById("visitedCountriesMap");
+					map.write(mapDiv);
 				});
     		}
 		}
