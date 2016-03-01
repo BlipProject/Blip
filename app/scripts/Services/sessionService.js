817e5a34f98f4682e6ff95a7c79968882bbc35bd
Brian Geraghty <brian_geraghty@outlook.com>
2016-03-01 20:13:46 +0000

    Added new button classes

---
+++ app/scripts/Services/sessionService.js
@@ -0,0 +1,17 @@
+angular.module('blipApp')
+	.service('SessionService', function ($http,$q) {
+		return{
+			SessionService: function(){
+				var deferred = $q.defer();
+
+				$http.get('http://localhost/blip/app/phpCore/session.php')
+					.success(function (data, status, headers, config) {
+		            	deferred.resolve(data);
+		         	})
+		         	.error(function(data, status, headers, config){
+		            	deferred.reject("An error occured");
+		        	});
+			    return deferred.promise;
+			}
+		};
+	});
