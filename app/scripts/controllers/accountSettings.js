0707feb328ddfa8161e393441124b4704cf907be
Fergal Gaffney <s00147036@mail.itsligo.ie>
2016-03-05 06:57:04 +0000

    Account Settings - some styling done

--- app/scripts/controllers/accountSettings.js
+++ app/scripts/controllers/accountSettings.js
@@ -1,5 +1,17 @@
 'use strict';
 
 angular.module('blipApp')
-  .controller('AccountSettingsCtrl', function () {
-  });
+  .controller('AccountSettingsCtrl', ['$http',
+  	'$scope',
+  	'$rootScope',
+  	function ($http, $scope, $rootScope) {
+
+  		$scope.loadNationalities = function() {
+            if(localStorage.getItem("cacheNat") === null) {
+                NationalityService.getNationalities().then(function(data){
+                    $scope.nationalities = JSON.parse(localStorage.cacheNat)
+                });
+            }
+            else { $scope.nationalities = JSON.parse(localStorage.cacheNat) };
+        };
+  }]);
