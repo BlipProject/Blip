85378bc26bca6f2eec9dace932aea47adc87cdc1
Brian Geraghty <brian_geraghty@outlook.com>
2016-03-10 18:26:29 +0000

    Cleanup code

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -4,7 +4,8 @@
 
          var tUp;
          //get language id from session
-         var language = ($rootScope.userCountryCodeCookie).toLowerCase();
+         //var language = ($rootScope.userCountryCodeCookie).toLowerCase();
+         var language = 'en';
          $scope.userCountry = $rootScope.userCountryCookie;
          //get user id from session
          var userId = $rootScope.userIdCookie;
